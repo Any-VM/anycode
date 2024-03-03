77659db72d6fdeb2b6307c95b53e70fede21e5e2
incognitotgt <tgt@incognitotgt.me>
2024-03-03 00:07:48 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,20 @@
-import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
   const projects = [0, 0, 0, 0, 0];
   return (
-    <div className="py-6 px-12 space-y-6">
+    <div className="space-y-6 px-12 py-6">
       <h1 className="text-3xl font-semibold">My Workers</h1>
       <div className="flex flex-wrap gap-4">
         {projects.map(() => {
           return (
-            <Card className="w-72 hover:border-white/50 duration-100 cursor-pointer">
+            <Card className="w-72 cursor-pointer duration-100 hover:border-white/50">
               <CardHeader>
                 <CardTitle className="cursor-pointer">worker-1</CardTitle>
                 <CardDescription>
@@ -26,7 +23,7 @@ export default function Home() {
               </CardHeader>
               <CardFooter className="flex items-center justify-between">
                 <div className="flex items-center gap-2 text-gray-200">
-                  <div className="w-3 h-3 rounded-full bg-red-400"></div>{" "}
+                  <div className="h-3 w-3 rounded-full bg-red-400"></div>{" "}
                   Offline
                 </div>
               </CardFooter>
