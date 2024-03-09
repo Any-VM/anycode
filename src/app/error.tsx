a20d57c5cd5cb83979741800dddf263cabef1188
anshnk <ansh@anshnk.dev>
2024-03-09 14:07:14 -0600

    Error Pages (not sure if functional)

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,2 +1,23 @@
 "use client"
+import React from 'react';
+import { NextPage, NextPageContext } from 'next';
 
+interface ErrorProps {
+  statusCode: number;
+}
+
+const ErrorPage: NextPage<ErrorProps> = ({ statusCode }) => {
+  return (
+    <div>
+      <h1>Error {statusCode}</h1>
+      <p>Oops! Something went wrong.</p>
+    </div>
+  );
+};
+
+ErrorPage.getInitialProps = ({ res, err }: NextPageContext): ErrorProps => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode: statusCode || 404 }; // Provide a default value
+};
+
+export default ErrorPage;
