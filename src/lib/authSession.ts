10b2d0f4424a558f5e09b3b57590cbd28da6b657
incognitotgt <tgt@incognitotgt.me>
2024-03-17 01:18:16 -0400

    remvoed unedded file

--- src/lib/authSession.ts
+++
@@ -1,6 +0,0 @@
-import { getServerSession } from "next-auth";
-import config from "@/auth.config";
-export default async function getSession() {
-  const session = await getServerSession(config);
-  return session;
-}
